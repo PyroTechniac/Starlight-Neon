fd1cfae9b93809bec1228b6774b774163c8e60a2
Gryffon Bellish <owenbellish@gmail.com>
2019-10-11 14:24:17 -0400

    finish initial commit

--- native.d.ts
+++ native.d.ts
@@ -1,1 +1,4 @@
+// This file is a declaration file for the native functions and exports from Neon, as TypeScript cannot
+// recognize a file ending in .node as a module.
+
 export declare function hello(): string;
